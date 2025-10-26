f0d5d0f5d58b5f56e1a7692f96772154db1b8723
JHKoder <jeonghun.kang.dev@gmail.com>
2025-10-27 06:24:28 +0900

    :sparkles: rest doc 설정 추가
    - 상위 index(소개), 중위 index(도메인) 구분

--- modules/bootstrap/api-payment-gateway/build.gradle.kts
+++ modules/bootstrap/api-payment-gateway/build.gradle.kts
@@ -1,10 +1,12 @@
+plugins {
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
+}
+
 tasks.jar {
     enabled = false
 }
 
-tasks.bootJar {
-    enabled = true
-}
+val asciidoctorExt by configurations.creating
 
 dependencies {
     implementation(projects.modules.domain)
@@ -18,6 +20,59 @@ dependencies {
     testImplementation(libs.spring.boot.starter.test) {
         exclude(module = "mockito-core")
     }
-    testImplementation(libs.spring.mockk)
+
     testImplementation(libs.database.h2)
+
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    testImplementation("org.springframework.restdocs:spring-restdocs-restassured")
+    testImplementation("org.springframework.restdocs:spring-restdocs-webtestclient")
+
+    asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
+}
+
+// REST Docs 설정
+val snippetsDir = file("build/generated-snippets")
+
+tasks.test {
+    outputs.dir(snippetsDir)
+}
+
+tasks.asciidoctor {
+    dependsOn(tasks.test)
+    configurations(asciidoctorExt.name)
+
+    setSourceDir(file("docs/asciidoc"))
+    setBaseDir(file("docs/asciidoc"))
+
+    sources {
+        include("**/index.adoc")
+    }
+
+    attributes(
+        mapOf("snippets" to snippetsDir)
+    )
+
+    doLast {
+        println("Asciidoctor output directory: ${outputDir.absolutePath}")
+    }
+}
+
+tasks.register("copyHTML", Copy::class) {
+    dependsOn(tasks.asciidoctor)
+    from(file("build/docs/asciidoc"))
+    into(file("src/main/resources/static/docs"))
+
+    doLast {
+        println("Copied HTML files to src/main/resources/static/docs")
+    }
+}
+
+tasks.bootJar {
+    enabled = true
+    dependsOn(tasks.getByName("copyHTML"))
+}
+
+tasks.build {
+    dependsOn(tasks.getByName("copyHTML"))
 }
