e694bd37f0f2d541687704337064014fe7238d80
JHKoder <jeonghun.kang.dev@gmail.com>
2025-10-27 07:02:31 +0900

    :recycle: 암호화 테스트 수정

--- modules/external/pg-client/src/test/kotlin/im/bigs/pg/external/pg/util/SecurePaymentEncoderTest.kt
+++ modules/external/pg-client/src/test/kotlin/im/bigs/pg/external/pg/util/SecurePaymentEncoderTest.kt
@@ -1,5 +1,7 @@
 package im.bigs.pg.external.pg.util
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import im.bigs.pg.external.pg.client.`in`.PgApiKeyRequest
 import im.bigs.pg.external.util.SecurePaymentEncoder
 import org.junit.jupiter.api.DisplayName
@@ -9,17 +11,23 @@ import kotlin.test.assertEquals
 
 class SecurePaymentEncoderTest {
 
+    private val mapper = jacksonObjectMapper()
+
     @Test
     @DisplayName("암호화 성공")
     fun `암호화 성공`() {
+        // Given
+        val apiKey = "11111111-1111-4111-8111-111111111111"
+        val iv = "AAAAAAAAAAAAAAAA"
         val request = PgApiKeyRequest("1111-1111-1111-1111", "19900101", "1227", "12", BigDecimal.valueOf(1000))
 
-        val result =
-            SecurePaymentEncoder.encryptToEnc("11111111-1111-4111-8111-111111111111", "AAAAAAAAAAAAAAAA", request)
+        // When
+        val encrypted = SecurePaymentEncoder.encryptToEnc(apiKey, iv, request)
 
-        assertEquals(
-            result,
-            "FlrQ_ZFCA9WC7HIkPzKFpnzv1AX0n7zodWtWRo6X6-ccrzwEkwOGAJyfC4cwihNy4EtwXS6yx2FBHcOP44mxDAZvv38YF6LLnBSBW2zpsvBUgImnuR6Gc_z1CTID_tuA-Rpmrhjoguyl3PxnF9A5dhTLM6T0HO4JxbA"
-        )
+        // Then
+        val decrypted = SecurePaymentEncoder.decrypt(apiKey, iv, encrypted)
+        val decryptedRequest = mapper.readValue<PgApiKeyRequest>(decrypted)
+
+        assertEquals(request, decryptedRequest)
     }
 }
